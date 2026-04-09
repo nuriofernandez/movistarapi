0f7badcc0b14595cf88c890fed71ff573ce495bc
Nurio Fernandez <xxnurioxx@gmail.com>
2026-04-09 06:55:53 +0200

    Added login capability

---
+++ integration/login/login.go
@@ -0,0 +1,53 @@
+package login
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+// Generate a 9 digits random number to be used as a sessionId
+func randomSessionId() string {
+	minLimit := int(math.Pow10(9))
+	maxLimit := int(math.Pow10(9 - 1))
+	randInt := int(rand.Float64() * float64(minLimit))
+	if randInt < maxLimit {
+		randInt += maxLimit
+	}
+	return fmt.Sprint(randInt)
+}
+
+func Login(pass string) (string, error) {
+	// Generate a random sessionId
+	sessionId := randomSessionId()
+
+	// Prepare login form
+	data := url.Values{}
+	data.Set("loginPassword", pass)
+
+	// Prepare request
+	req, err := http.NewRequest("POST", "http://192.168.1.1/te_acceso_router.cgi", strings.NewReader(data.Encode()))
+	if err != nil {
+		return "", err
+	}
+	req.AddCookie(&http.Cookie{Name: "sessionID", Value: sessionId})
+
+	// Execute the request
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	// Validate status code
+	if res.StatusCode != 200 {
+		return "", errors.New(res.Status)
+	}
+
+	// Return original sessionId
+	return sessionId, nil
+}
--- main.go
+++ main.go
@@ -2,12 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/xXNurioXx/movistar-router-api/integration/login"
 	"github.com/xXNurioXx/movistar-router-api/integration/mapalocal"
 )
 
 func main() {
-	fmt.Println("HEY!")
-	devices, err := mapalocal.GetLocalDevices("199970505")
+	fmt.Println("Welcome to the Unofficial Movistar router control CLI!")
+
+	// Ensure the user actually typed something
+	if len(os.Args) < 2 {
+		fmt.Println("Invalid command")
+		fmt.Println("Usage: binary <router-password>")
+		return
+	}
+
+	// Store router password
+	password := os.Args[1]
+
+	// Prepare login
+	fmt.Println("Logging in...")
+	sessionId, err := login.Login(password)
+	if err != nil {
+		fmt.Println("Unable to login, " + err.Error())
+		return
+	}
+	fmt.Println("Successfully logged in")
+
+	// Prepare local map retrieve
+	fmt.Println("Retrieving 'Local Map'...")
+	devices, err := mapalocal.GetLocalDevices(sessionId)
 	if err != nil {
 		return
 	}
