6ca7454e66f129924ab56ad9d8e95a44ca1bb7a0
Nurio Fernandez <xxnurioxx@gmail.com>
2026-05-13 02:53:37 +0200

    Moved OpenPort api logics to 'api' pkg

--- hgu/portopen.go
+++ hgu/portopen.go
@@ -2,9 +2,8 @@ package hgu
 
 import (
 	"fmt"
-	"io"
-	"net/http"
-	"strings"
+
+	"github.com/nuriofernandez/movistarapi/api"
 )
 
 func (h *HGUSession) OpenPort(port OpenPort) error {
@@ -18,34 +17,6 @@ func (h *HGUSession) OpenPort(port OpenPort) error {
 		return err
 	}
 
-	req, err := http.NewRequest("GET", "http://192.168.1.1/te_ppp_pm.cmd?action=create&inst="+portValue+"&sessionKey="+h.sessionId, nil)
-	if err != nil {
-		return err
-	}
-
-	req.AddCookie(&http.Cookie{Name: "sessionID", Value: h.sessionId})
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-
-	// We read the response body on the line below.
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	//Convert the body to type string
-	stringBody := string(body)
-	removeScript := strings.Split(stringBody, "</script>\n")[1]
-
-	responseOk := strings.Contains(removeScript, "inst=true")
-	if !responseOk {
-		return fmt.Errorf("failed to perform operation, movistar denied it")
-	}
-
-	// All right!
-	return nil
+	// Forward it to api.OpenPort
+	return api.OpenPort(h.sessionId, portValue)
 }
